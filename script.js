0066a4c69fa643c38f493af2ded236f05523228e
kghs114450 <s114450@ms2.kghs.kh.edu.tw>
2026-06-15 08:35:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("StudyNest 啟動成功");
+const events = {
+    12: "📚 數學讀書會",
+    15: "🧪 科展會議",
+    20: "📖 英檢模考"
+};
+
+const days = document.querySelectorAll(".day");
+
+days.forEach(day => {
+
+    const dayNumber = parseInt(day.textContent);
+
+    if(events[dayNumber]){
+
+        day.innerHTML = `
+            <div>${dayNumber}</div>
+            <div class="event">
+                ${events[dayNumber]}
+            </div>
+        `;
+
+    }
+
+});
